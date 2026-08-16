b8ca2474909a49aeff6c2138df560d3984d747a6
clutchmuffin <syedfarzaanrafi@gmail.com>
2026-08-15 18:21:41 -0600

    Used OpenRouter API to test out a message

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import os
 from dotenv import load_dotenv
+from openai import OpenAI
 
 load_dotenv()
 api_key = os.environ.get("OPENROUTER_API_KEY")
 
-def main():
-    print("Hello from ai-agent!")
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
 
+messages = [
+    {
+        "role": "user",
+        "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    }
+]
 
-if __name__ == "__main__":
-    main()
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=messages
+)
+
+print(response.choices[0].message.content)
