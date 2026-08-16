aa27278a22c86785a4817256c9e6cbdd13a08f90
clutchmuffin <syedfarzaanrafi@gmail.com>
2026-08-16 17:37:51 -0600

    Added function 'file_information_builder' that lists the information of the files in the given directory

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -17,7 +17,26 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         if not valid_target_directory:
             return f"Error: Cannot list '{directory}' as it is outside the permitted working directory"
         else:
-            return f"Success: '{directory} is within the working directory'"
+            # return f"Success: '{directory} is within the working directory'"
+            return file_information_builder(target_directory)
     
+    except Exception as e:
+        return f"Error: '{e}'"
+
+def file_information_builder(directory: str) -> str:
+    contents = ""
+
+    try:
+        files_in_directory = os.listdir(directory)
+        for file in files_in_directory:
+            file_name = file
+            file_path = directory + "/" + file
+            file_size = os.path.getsize(file_path)
+            is_file_dir = os.path.isdir(file_path)
+
+            contents += f"- {file_name}: file_size={file_size}, is_dir={is_file_dir}\n"
+
+        return contents
+
     except Exception as e:
         return f"Error: '{e}'"
